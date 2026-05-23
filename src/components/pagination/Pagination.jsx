afbc2171229b71cf55dda3b4f754afafc5c97351
kiran-kharsel <kirankharsel@gmail.com>
2026-05-23 11:32:57 +0530

    feat: fetch data

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './style.css'
 
 function Pagination() {
+    const [items, setItems] = useState([]);
+
+
+    useEffect(()=>{
+        fetch('https://dummyjson.com/recipes?limit=0&select=name,image')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            setItems(data.recipes)
+        })
+        .catch(err => console.error(err))
+    }, []);
+
   return (
-    <div>Pagination</div>
+    <div>
+        <div className="content">
+            {/* {items[0].name} */}
+            {items.map((item) => {
+                return (
+                    <div key={item.id}>{item.name}</div>
+                )
+            })}
+        </div>
+        <div className="pages"></div>
+    </div>
   )
 }
 
